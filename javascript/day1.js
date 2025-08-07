81706ad95c7ed6e6036f59d6a387053533724867
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-08-07 21:58:02 +0500

    JS CODE PUSHED

---
+++ javascript/day1.js
@@ -0,0 +1,42 @@
+console.log("21312312312")
+
+
+var num1 = 10;
+var num2 = 20;
+
+console.log(num1)
+
+
+var total = num1 + num2;
+
+console.log(num1 + num2)
+
+
+console.log("70" + "30")
+// 1- String 
+
+
+
+
+console.log(40 + 60)
+// 2 - Number
+
+
+
+console.log(true)
+// 3 - Boolean
+
+
+console.log("Hello" + 20)
+
+var jawwad = "Hello World"
+
+console.log(jawwad)
+
+
+var dob = "10th December, 2025"
+
+console.log(dob)
+
+
+console.log("20" - "10")
