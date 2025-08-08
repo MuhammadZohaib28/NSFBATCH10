ae111b855e0c9eb4928ae664ffcedb6af2cf96af
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-08-08 22:09:59 +0500

    DAY 2

---
+++ javascript/day2.js
@@ -0,0 +1,75 @@
+console.log("Hello World");
+
+// 1 - var
+// 2 - let
+// 3 - const
+
+var fullName = "Muhammad Zohaib";
+
+var fullName = "Hunain";
+
+fullName = "Maria"; //UPDATE
+
+console.log(fullName);
+
+console.log(typeof fullName);
+
+{
+  let cityName = "Karachi";
+  console.log(cityName);
+
+  var countryName = "Pakistan";
+}
+
+console.log(countryName);
+
+{
+  let cityName = "Lahore";
+
+  cityName = "Peshawar";
+
+  console.log(cityName);
+}
+
+const stateName = 67;
+
+// stateName = "KPK";
+
+console.log(stateName);
+
+
+
+var studentAge = 21;
+
+// let studentAge = 24 
+
+{
+    
+    console.log(studentAge)
+}
+
+
+// PRIMITIVE DATA TYPE
+
+// 1 - STRING
+// 2 - NUMBER
+// 3 - BOOLEAN
+// 4 - UNDEFINED
+// 5 - NULL
+// 6 - BIGINT
+
+let counting = 10000000000000000000000000000000000000000000000000000000000000;
+
+console.log(BigInt(counting))
+
+
+
+let name1 = ""
+
+
+let name = null;
+
+
+
+let meraNaam;
+console.log(meraNaam)
