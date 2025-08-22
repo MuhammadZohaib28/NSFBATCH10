4ffebf70f0059aacf43c0cf7b782b143e0c0e217
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-08-22 22:04:05 +0500

    ARRAYS

---
+++ javascript/day7.js
@@ -0,0 +1,57 @@
+//ARRAYS IN JAVASCRIPT
+
+let fruit1 = "Mango";
+let fruit2 = "Banana";
+let fruit3 = "Guvava";
+let fruit4 = "Apple";
+
+let fruitsName = [
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  "Mango",
+  "Banana",
+  "Guvava",
+  42423,
+  "Pineapple",
+];
+
+console.log(fruitsName.length);
+
+fruitsName.push("Orange");
+
+fruitsName.pop();
+
+
+fruitsName.
+
+for (let i = 1; i <= fruitsName.length - 1; i++) {
+  console.log(`SNO: ${i + 1} - ${fruitsName[i]}`);
+}
